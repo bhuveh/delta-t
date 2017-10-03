b502e505f196035564511bc544287901a0b76fb6
Bhuvi Gupta <bhuvi.92@gmail.com>
2017-10-03 13:45:04 +0530

    Adds comfort slider, collapsible sidebar, and template pages for all menu items.

--- script/index.js
+++ script/index.js
@@ -1,5 +1,5 @@
 (function(){
-  var app = angular.module('dtApp', [ 'ngRoute' ]);
+  var app = angular.module('dtApp', [ 'ngRoute', 'rzModule' ]);
   
   app.controller('DTController', ['$scope', function($scope) {
     $scope.coordinators = ['0001','0002','0003'];
@@ -9,6 +9,11 @@
       $scope.id = id;
       //console.log('ID set to ' + id);
     };
+    
+    $scope.sidebar = false;
+    $scope.toggleSidebar = function() {
+      $scope.sidebar = !$scope.sidebar;
+    }
   }]);
   
   app.controller('SidebarController', ['$scope', '$sce', function($scope, $sce) {
@@ -38,9 +43,38 @@
         key: 'plan'
       }
     ];
-    
-    $scope.activate = function() {
-      $('#sidebar').toggleClass('active');
+  }]);
+  
+  app.controller('VoteController', ['$scope', function($scope) {
+    $scope.slider = {
+      val: 0,
+      opt: {
+        floor: -300,
+        ceil: 300,
+        step: 1,
+        vertical: true,
+        translate: function(value) {
+          return value/100;
+        },
+        showTicks: 100,
+        getLegend: function(value) {
+          if(value==-300)
+            return 'Cold';
+          if(value==-200)
+            return 'Cool';
+          if(value==-100)
+            return 'Slightly Cool';
+          if(value==0)
+            return 'Neutral';
+          if(value==100)
+            return 'Slightly Warm';
+          if(value==200)
+            return 'Warm';
+          if(value==300)
+            return 'Hot';
+          return null;
+        },
+      }
     };
   }]);
   
@@ -48,6 +82,24 @@
     return {
       restrict: 'E',
       templateUrl: './templates/directives/login.html'
+    };
+  });
+  
+  app.directive('topbar', function() {
+    return {
+      restrict: 'E',
+      templateUrl: './templates/directives/topbar.html'
+    };
+  });
+  
+  app.directive('sidebar', function() {
+    return {
+      restrict: 'E',
+      templateUrl: './templates/directives/sidebar.html',
+      controller: 'SidebarController',
+      scope: {
+        id: '@'
+      }
     };
   });
 })();
--- script/routes.js
+++ script/routes.js
@@ -9,5 +9,23 @@ angular.module('dtApp')
     .when('/home', {
       templateUrl: './templates/pages/home.html'  
     })
+    .when('/vote', {
+      templateUrl: './templates/pages/vote.html'  
+    })
+    .when('/bill', {
+      templateUrl: './templates/pages/bill.html'  
+    })
+    .when('/cdata', {
+      templateUrl: './templates/pages/cdata.html'  
+    })
+    .when('/idata', {
+      templateUrl: './templates/pages/idata.html'  
+    })
+    .when('/edata', {
+      templateUrl: './templates/pages/edata.html'  
+    })
+    .when('/plan', {
+      templateUrl: './templates/pages/plan.html'  
+    })
     .otherwise( { redirectTo: '/' });
 });
